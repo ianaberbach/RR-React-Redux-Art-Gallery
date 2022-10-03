fab5d0a8ec87cdca97d6eb48df9222eaabdb8535
Ian <naberbach@gmail.com>
2022-10-03 17:45:38 -0400

    finihsed activity

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import './App.css';
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import { useEffect } from 'react';
+import { useSelector, useDispatch, connect } from 'react-redux';
+import {fetchData, nextImage, prevImage, setArtId, reset} from './features/dataSlice';
 
-function App() {
-  
+const mapStateToProps = (state) => ({
+  artId: state.data.artId
+})
+
+function App(props) {
+  const dispatch = useDispatch();
+  const currentState = useSelector(state => state.data);
+
+  useEffect(() => {
+    dispatch(fetchData())
+  },[props.artId, dispatch])
+
+  const renderImage = () => {
+    return currentState.apiData ?
+    <img src={currentState.apiData.primaryImage} /> :
+    <h3>No Image Found</h3>
+  }
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
-      <Nav />
-      <ContentWrapper />
-      <Footer />
+    <div className="App">
+      <div>
+        <button onClick={() => {dispatch(fetchData())}}>Trigger Thunk</button>
+        <button onClick={() => {dispatch(reset())}}>Clear</button>
+        <button onClick={() => {dispatch(nextImage())}}>Next</button>
+        <button onClick={() => {dispatch(prevImage())}}>Back</button>
+      </div>
+      <h1>{currentState.artId}</h1>
+      <input value={currentState.artId} onChange={(e) => {dispatch(setArtId(e.target.value))}} />
+      <div>
+        {
+          renderImage()
+        }
+      </div>
     </div>
   );
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
